ece3a0aef3570e8f4791ff2795da770d5c834ff0
Charlie Wood <charliemowood@gmail.com>
2017-06-20 21:38:29 +0100

    Add working map markers extend

--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -7,17 +7,38 @@ var MapWrapper = function (location, coords, zoom) {
   });
 }
 
+
+
+
 MapWrapper.prototype = {
   addMarker: function(coords) {
-    var market = new google.maps.Marker({
+    var marker = new google.maps.Marker({
       position: coords,
       map: this.googleMap
+      // info: infowindow.open(marker, coords);
     })
+
+
+
+
+
+
   },
   addClickEvent: function () {
     this.googleMap.addListener('click', function(event) {
       var coords = { lat: event.latLng.lat(), lng: event.latLng.lng()}
       this.addMarker(coords)
+
+      // var contentString = '<div id="content">'+
+      //  '<h1>hello</h1>'+
+      //  '</div>'
+      // var infowindow = new google.maps.InfoWindow({
+      //   content: contentString
+      // })
+      //
+      // infowindow.open(marker, coords);
+      //
+      // console.log(this);
     }.bind(this));
   }
 }
